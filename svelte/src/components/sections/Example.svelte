62e4789f97946a7dc5f81ee23226455107667641
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-10-05 17:16:39 +1100

    renaming PortableText

--- svelte/src/components/sections/Example.svelte
+++ svelte/src/components/sections/Example.svelte
@@ -1,10 +1,10 @@
 <script>
-	import RichText from '~components/RichText.svelte';
+	import PortableText from '~components/PortableText.svelte';
 
 	export let data;
 </script>
 
 <div class="relative flex flex-col">
 	<h1 class="text-lg font-bold">{data.title}</h1>
-	<RichText value={data?.body} />
+	<PortableText value={data?.body} />
 </div>
