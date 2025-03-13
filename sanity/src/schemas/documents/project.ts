28ad103f09ca074f538f03a7ecca133db939864b
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-03-13 16:16:42 +1100

    feat: Add validation rule to project schema for required fields

--- sanity/src/schemas/documents/project.ts
+++ sanity/src/schemas/documents/project.ts
@@ -39,6 +39,7 @@ export default defineType({
       options: {
         dateFormat: 'YYYY-MM-DD',
       },
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'thumbnail',
