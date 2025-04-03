52b308679a83e0178929e430d9adeec399cdcb48
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-04-03 15:02:40 +1100

    feat: Refactor Image component to use SanityImageData type and improve image loading handling

--- svelte/src/lib/components/Image.svelte
+++ svelte/src/lib/components/Image.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-	import type { RawImage } from '$lib/types';
+	import { twMerge } from 'tailwind-merge';
+	import type { SanityImageData } from '$lib/types';
 	import { getImageProps } from '$lib/utils/sanity/client/image';
 
 	interface Props {
-		image: RawImage;
+		class?: string;
+		image: SanityImageData;
+		alt?: string;
 		loading?: 'lazy' | 'eager';
 		maxWidth?: number;
 	}
 
-	let { image, loading = 'lazy', maxWidth }: Props = $props();
-
+	const { image, alt = '', class: className, loading = 'lazy', maxWidth }: Props = $props();
 	let img = getImageProps({ image, maxWidth });
 	let loaded = $state(false);
+
+	const handleImageLoaded = () => {
+		loaded = true;
+	};
 </script>
 
 {#if img}
-	<figure class="relative overflow-hidden">
-		{#if img.placeholder}
-			<img class="absolute inset-0 h-full w-full" src={img.placeholder} alt="" aria-hidden="true" />
-		{/if}
+	<figure class={twMerge('relative w-full', className)}>
 		<img
 			class={[
 				'relative h-auto w-full transition-opacity duration-700',
@@ -27,12 +30,23 @@
 			{loading}
 			fetchPriority={loading === 'eager' ? 'high' : undefined}
 			src={img.src}
-			srcset={img.srcset}
+			srcset={img.srcSet}
 			sizes={img.sizes}
 			width={img.width}
 			height={img.height}
-			alt={img.alt}
-			onload={() => (loaded = true)}
+			alt={alt || img.alt}
+			onload={handleImageLoaded}
 		/>
+		{#if img.placeholder}
+			<img
+				class={twMerge(
+					'pointer-events-none absolute inset-0 block h-full w-full object-cover transition-opacity',
+					loaded ? 'opacity-0' : 'opacity-100'
+				)}
+				src={img.placeholder}
+				alt=""
+				aria-hidden="true"
+			/>
+		{/if}
 	</figure>
 {/if}
